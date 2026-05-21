249c8ddd2c0c91ab488bda58fbe9d98d8a740e61
Antonio Garcia Martinez <antoniogarciam98@gmail.com>
2026-05-21 12:05:02 +0200

    Delete HuerteandoApp/Android/HuerteandoApp directory
    Carpeta duplicada

--- HuerteandoApp/Android/HuerteandoApp/Backend/src/main/resources/db/reset.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS huerteando.comentario;
-DROP TABLE IF EXISTS huerteando.megusta;
-DROP TABLE IF EXISTS huerteando.imagen;
-DROP TABLE IF EXISTS huerteando.observacion;
-DROP TABLE IF EXISTS huerteando.especie;
-DROP TABLE IF EXISTS huerteando.tipo_observacion;
-DROP TABLE IF EXISTS huerteando.catalogo_eei;
-DROP TABLE IF EXISTS huerteando.usuario;
